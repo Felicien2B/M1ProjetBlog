17d05e90a9e4665177c96f6c2354911216a358e1
Felicien2B <90379875+Felicien2B@users.noreply.github.com>
2022-12-12 00:21:14 +0100

    Ajout copie images

--- generer_site.sh
+++ generer_site.sh
@@ -3,5 +3,9 @@ commande="$1"
 repertoire_cible="$2"
 case "$commande" in
     "build" )
+	if [ ! -d "$repertoire_cible" ]
+	then mkdir "$repertoire_cible"
+	fi
+	cp -r images "$repertoire_cible/images"
         echo "Test">"$2/index.html";;
 esac

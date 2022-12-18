e43fa42609c366db8b243d83d8411b18bc6d583f
Felicien2B <90379875+Felicien2B@users.noreply.github.com>
2022-12-18 10:24:10 +0100

    Ajout et copie index.html

--- generer_site.sh
+++ generer_site.sh
@@ -3,9 +3,13 @@ commande="$1"
 repertoire_cible="$2"
 case "$commande" in
     "build" )
-	if [ ! -d "$repertoire_cible" ]
-	then mkdir "$repertoire_cible"
-	fi
-	cp -r images "$repertoire_cible/images"
-        echo "Test">"$2/index.html";;
+	if [ -f index.html ]
+	then
+		if [ ! -d "$repertoire_cible" ]
+		then mkdir "$repertoire_cible"
+		fi
+		cp -r images "$repertoire_cible"
+		cp index.html "$repertoire_cible/index.html"
+	else echo "Fichier manquant"
+	fi;;
 esac

f1a7a8cf68e72c4cecd09ae156ffd8d9c18345cf
Jean2B <90519788+Jean2B@users.noreply.github.com>
2022-12-18 14:32:05 +0100

    Intégration des images dans index.html

--- generer_site.sh
+++ generer_site.sh
@@ -5,11 +5,21 @@ case "$commande" in
     "build" )
 	if [ -f index.html ]
 	then
+		if [ $(cat index.html | grep -o "id=\"images\"" | wc -l) -eq "0" ]
+		then echo "L'ID images n'existe pas dans index.html"
+		else
 		if [ ! -d "$repertoire_cible" ]
 		then mkdir "$repertoire_cible"
 		fi
 		cp -r images "$repertoire_cible"
 		cp index.html "$repertoire_cible/index.html"
+		html_images=""
+		for fichier in images/*
+		do
+			html_images="$html_images<img src='$fichier'>"
+		done
+		sed -i "/id=\"images\"/a $html_images" "$repertoire_cible/index.html"
+		fi
 	else echo "Fichier manquant"
 	fi;;
 esac
